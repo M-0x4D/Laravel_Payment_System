228adae73d4f8f77fa435076f570a4836c22504a
M-0x4D <adl774855@gmail.com>
2022-09-16 09:22:06 -0700

    secend

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -19,9 +19,6 @@ class Authcontroller extends Controller
 
     function register(Request $request)
     {
-
-       // mohamed_validation();
-
         $validator = validator()->make($request->all() , [
             'name' => 'required',
             'email' => 'required',
@@ -29,17 +26,11 @@ class Authcontroller extends Controller
             'password' => ['required' , ],
             'city_id' => 'required'
         ]);
-
-       
-
         if ($validator->fails()) {
             # code...
-             return response()->json("test");
-        }
-
+             return returnjson(0 , 'failed' , 'no data');
+            }
         else {
-            
-        
         $phone = Client::where('phone' , $request->phone)->first();
         if($phone == NULL)
         {
@@ -48,83 +39,60 @@ class Authcontroller extends Controller
         $client->api_token = Str::random(60);
         $client->save();
         $client->client_role()->attach(4 , ['model_type' => 'test' , 'model_id' => $client->id]);
-        return $client;
-
+        return returnjson(1 , 'success' , $client);
         }
         else
         {
-            return response()->json(['msg' => 'failed']);
+            return returnjson(0 , 'failed' , 'no data');
+        }   
         }
-            
-        }
-        
+     
     }
+
+
+
+
 
 
 
     function login(Request $request)
     {
-
         $validator = validator()->make($request->all() , [
 
             'phone' => 'required',
-            'password' => 'required',
-            
+            'password' => 'required',    
         ]);
-
-
         if ($validator->fails()) {
             # code...
-             return response()->json(['msg'=>"failed"]);
+             return returnjson(0 , 'failed' , 'no data');
         }
-
-
-
-        if (Auth::guard('client_web')->validate(['phone' => $request->phone , 'password' => $request->password])) {
-
-            # code...
+        if (Auth::guard('api_web')->validate(['phone' => $request->phone , 'password' => $request->password])) {
             $client = Client::where('phone' , $request->phone)->first();
-
-            return response()->json(['token' =>  $client->api_token ,'msg' => $client]);
-
-        }
+            return returnjson(1,'success',['token' =>  $client->api_token ,'msg' => $client]);
+            }
         else {
-            
-            return response()->json(['msg' => 'not log in ']);
+            return returnjson(0,'failed','no log in ');
         }
-   /////////////////////////////////////////////////////////////     
         $client = Client::where('phone' , $request->phone)->first();
         if($client)
         {
             if(Hash::check($request->password , $client->password))
             {
-                return response()->json(['api_token' => $client->api_token , 'user'=>$client]);
+                return returnjson(1,'success',['api_token' => $client->api_token , 'user'=>$client]);
 
-            }
-            
+            }    
         }
         else
         {
-            return response()->json(['user'=> "no user"]);
-        }
-
-        // $check = auth()->validate($request->all());
-
-        // if($check)
-        // {
-        //     return auth()->api_tokem;
-        // }
-        // else
-        // {
-        //     return "failed";
-        // }
-        
-         
-            
-        
-        
-       
+            return  returnjson(0,'failed',"no user");
+        }  
     }
+
+
+
+
+
+
 
     function reset_password(Request $request)
     {
@@ -135,16 +103,20 @@ class Authcontroller extends Controller
         if ($update) {
             # code...
             Mail::to(env('MAIL_LOG_CHANNEL'))->send(new ResetPassword($code));
-        return response()->json(['code' => 'password reset with code : ' . $code , 'fails' => Mail::failures()]);
+        return  returnjson(1,'success',['code' => 'password reset with code : ' . $code , 'fails' => Mail::failures()]);
         }
         else {
             
-            return "failed";
-        }
-        
-        
+            return returnjson(0,'failed','فشل في تعديل ال pin code');
+        }  
     }
 
+
+
+
+
+
+    
     function new_password(Request $request)
     {
         if ($request->pin_code == $request->user()->pin_code && $request->password === $request->confirm_password) {
@@ -152,11 +124,11 @@ class Authcontroller extends Controller
             
             $user = $request->user();
             $update = $user->update(['password' =>  bcrypt($request->password) , 'pin_code' => NULL ]);
-            return response()->json(['msg' => 'تم تغيير الباسوورد بنجاح']);
+            return returnjson(1,'success','تم تغيير الباسوورد بنجاح');
         }
         else {
             
-            return response()->json(['msg' => 'فشل في تغيير كلمه السر']);
+            return returnjson(0,'failed','فشل في تغيير كلمه السر');
         }
         
     }
